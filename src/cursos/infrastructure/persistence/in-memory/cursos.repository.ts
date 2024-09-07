8acde9c697571451f223334756b72121930c6df5
Ana Laura Nolasco <nolascolunardi@gmail.com>
2024-09-06 22:44:07 -0300

    refactor: modify enroll student function in Course repository

--- src/cursos/infrastructure/persistence/in-memory/cursos.repository.ts
+++ src/cursos/infrastructure/persistence/in-memory/cursos.repository.ts
@@ -1,31 +1,32 @@
-import {Injectable} from "@nestjs/common";
-import {CursoRepository} from "../../../application/ports/cursos.repository";
-import {Curso} from "../../../domain/curso";
-import {CursosData} from "./cursos.data";
+import { Injectable } from '@nestjs/common';
+import { CursoRepository } from '../../../application/ports/cursos.repository';
+import { Curso } from '../../../domain/curso';
+import { CursosData } from './cursos.data';
 
 @Injectable()
 export class InMemoryCursosRepository extends CursoRepository {
-    private readonly cursos: Curso[];
+  private readonly cursos: Curso[];
 
-    constructor(dataCursos: CursosData) {
-        super();
-        this.cursos = dataCursos.database;
-    }
+  constructor(dataCursos: CursosData) {
+    super();
+    this.cursos = dataCursos.database;
+  }
 
-    salvar(curso: Curso): Curso {
-        this.cursos.push(curso);
-        return curso;
-    }
+  salvar(curso: Curso): Curso {
+    this.cursos.push(curso);
+    return curso;
+  }
 
-    listar(): Curso[] {
-        return this.cursos;
-    }
+  listar(): Curso[] {
+    return this.cursos;
+  }
 
-    buscarPorTitulo(titulo: string): Curso {
-        return this.cursos.find(curso => curso.titulo === titulo);
-    }
+  buscarPorTitulo(titulo: string): Curso {
+    return this.cursos.find((curso) => curso.titulo === titulo);
+  }
 
-    buscarPorTurma(turma: string): Curso {
-        return this.cursos.find(curso => curso.turma === turma);
-    }
+  matricularAluno(alunoRA: string, curso: Curso): Curso {
+    curso.alunos.push(alunoRA);
+    return curso;
+  }
 }

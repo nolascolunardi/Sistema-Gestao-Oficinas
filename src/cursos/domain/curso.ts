fd1493efad2e2eed18e3f4df3c44339ee39abc68
Ana Laura Nolasco <nolascolunardi@gmail.com>
2024-09-06 22:41:19 -0300

    refactor: remove unnecessary attribute and method from Course

--- src/cursos/domain/curso.ts
+++ src/cursos/domain/curso.ts
@@ -1,30 +1,30 @@
-import {Aluno} from "../../alunos/domain/aluno";
-
 export class Curso {
-    id: string;
-    titulo: string;
-    turma: string;
-    descricao: string;
-    categoria: string;
-    professores: string[];
-    alunos: string[];
-    dataInicio: string;
-    dataFinal: string;
+  id: string;
+  titulo: string;
+  descricao: string;
+  categoria: string;
+  professores: string[];
+  alunos: string[];
+  dataInicio: string;
+  dataFinal: string;
 
-    constructor(id: string, titulo: string, turma: string, descricao: string, categoria: string, professores: string[], alunos: string[], dataInicio: string, dataFinal: string) {
-        this.id = id;
-        this.titulo = titulo;
-        this.turma = turma;
-        this.descricao = descricao;
-        this.categoria = categoria;
-        this.professores = professores;
-        this.alunos = alunos;
-        this.dataInicio = dataInicio;
-        this.dataFinal = dataFinal;
-    }
-
-    matricular(aluno: Aluno) {
-        this.alunos.push(aluno.RA);
-    }
-
+  constructor(
+    id: string,
+    titulo: string,
+    descricao: string,
+    categoria: string,
+    professores: string[],
+    alunos: string[],
+    dataInicio: string,
+    dataFinal: string,
+  ) {
+    this.id = id;
+    this.titulo = titulo;
+    this.descricao = descricao;
+    this.categoria = categoria;
+    this.professores = professores;
+    this.alunos = alunos;
+    this.dataInicio = dataInicio;
+    this.dataFinal = dataFinal;
+  }
 }
